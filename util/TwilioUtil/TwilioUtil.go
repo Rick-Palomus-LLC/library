74df67d0bb65b4a9182d835970cccace521679fd
Kevin <kevin@Kevins-MBP.fios-router.home>
2018-10-14 12:25:39 -0400

    switch to recent twilio-go

--- util/TwilioUtil/TwilioUtil.go
+++ util/TwilioUtil/TwilioUtil.go
@@ -3,17 +3,17 @@ package TwilioUtil
 import (
 	"os"
 
-	twilio "github.com/saintpete/twilio-go"
+	twilio "github.com/kevinburke/twilio-go"
 )
 
 const twilioUrlPart = "https://api.twilio.com/2010-04-01"
 const post = "POST"
 
-func SendMessage(body string) (string, error) {
+func SendMessage(body string) (*twilio.Message, error) {
 	twilioSid := os.Getenv("TWILIO_SID")
 	twilioToken := os.Getenv("TWILIO_TOKEN")
 
-	client := twilio.NewClient(twilioSid, twilioToken)
+	client := twilio.NewClient(twilioSid, twilioToken, nil)
 
-	return client.Messages.SendMessage(os.Getenv("SMS_NUMBER"), os.Getenv("TWILIO_NUMBER"), body)
+	return client.Messages.SendMessage(os.Getenv("SMS_NUMBER"), os.Getenv("TWILIO_NUMBER"), body, nil)
 }
