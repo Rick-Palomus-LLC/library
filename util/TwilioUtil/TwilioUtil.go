e131bacbb3cb2207b5702a2a2ce699696480ea89
Kevin <kevin@Kevins-MBP.fios-router.home>
2018-10-14 12:17:14 -0400

    switch to twilio-go client

--- util/TwilioUtil/TwilioUtil.go
+++ util/TwilioUtil/TwilioUtil.go
@@ -1,66 +1,19 @@
 package TwilioUtil
 
 import (
-	"fmt"
-	"net/http"
-	"net/url"
 	"os"
-	"strings"
-)
 
-type twilioApiData struct {
-	twilioSID   string
-	twilioToken string
-	twilioUrl   string
-}
+	twilio "github.com/saintpete/twilio-go"
+)
 
 const twilioUrlPart = "https://api.twilio.com/2010-04-01"
 const post = "POST"
 
 func SendMessage(body string) (string, error) {
-	params := getParams(body)
+	twilioSid := os.Getenv("TWILIO_SID")
+	twilioToken := os.Getenv("TWILIO_TOKEN")
 
-	requestBody := *strings.NewReader(params.Encode())
+	client := twilio.NewClient(twilioSid, twilioToken)
 
-	return sendRequest(requestBody)
-}
-
-func getParams(body string) url.Values {
-	params := url.Values{}
-
-	params.Set("To", os.Getenv("SMS_NUMBER"))
-	params.Set("From", os.Getenv("TWILIO_NUMBER"))
-	params.Set("Body", body)
-
-	return params
-}
-
-func getApiData() twilioApiData {
-	twilioSID := os.Getenv("TWILIO_SID")
-
-	return twilioApiData{
-		twilioSID:   twilioSID,
-		twilioToken: os.Getenv("TWILIO_TOKEN"),
-		twilioUrl:   fmt.Sprintf("%s/%s/Messages.json", twilioUrlPart, twilioSID),
-	}
-}
-
-func sendRequest(requestBody strings.Reader) (string, error) {
-	client := &http.Client{}
-
-	apiData := getApiData()
-
-	request, err := http.NewRequest(post, apiData.twilioUrl, &requestBody)
-
-	if err != nil {
-		return "", err
-	}
-
-	request.SetBasicAuth(apiData.twilioSID, apiData.twilioToken)
-	request.Header.Add("Accept", "application/json")
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	response, err := client.Do(request)
-
-	return response.Status, nil
+	return client.Messages.SendMessage(os.Getenv("SMS_NUMBER"), os.Getenv("TWILIO_NUMBER"), body)
 }
